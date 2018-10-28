0909bc71819a677e201e8e7a7d2932fbf75e098d
vladimir.abrahamyan96 <vladimir.abrahamyan96@gmail.com>
2018-10-29 01:47:29 +0400

    Creat Form Validation repository

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import {Component} from '@angular/core';
+import {NgForm} from '@angular/forms';
+
+@Component({
+    selector: 'app-root',
+    templateUrl: './app.component.html'
+})
+export class AppComponent {
+    answers = [{
+        type: 'yes',
+        text: 'Да'
+    }, {
+        type: 'no',
+        text: 'Нет'
+    }];
+
+    defaultAnswer = 'no';
+    defaultCountry = 'ru';
+
+    submitForm(form: NgForm) {
+        console.log(form);
+    }
+}
